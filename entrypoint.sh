15a7b885c9aff31b97892a54096e7237db75cbaf
zhasmin <yusu1pova@gmail.com>
2026-05-16 17:44:53 +0600

    Fix entrypoint remove nc check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-echo "Waiting for PostgreSQL..."
-while ! nc -z $DB_HOST $DB_PORT; do
-  sleep 1
-done
-echo "PostgreSQL is ready!"
-python manage.py migrate
+echo "Running migrations..."
+python manage.py migrate --noinput
+echo "Starting server..."
 python manage.py runserver 0.0.0.0:8001
